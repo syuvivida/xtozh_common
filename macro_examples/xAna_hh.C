2c954508fa40783b4c6749e4f30eadee1ea7b13c
Shin-Shan Yu <syu@ncu200.cern.ch>
2015-08-26 10:06:55 +0200

    example macros for Graviton->hh

---
+++ macro_examples/xAna_hh.C
@@ -0,0 +1,105 @@
+// example code to run Bulk Graviton->ZZ->ZlepZhad selections on electron-channel
+
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <TString.h>
+#include <map>
+#include <TH1D.h>
+#include <TFile.h>
+#include "untuplizer.h"
+#include <TClonesArray.h>
+#include <TLorentzVector.h>
+
+using namespace std;
+void xAna_hh(std::string inputFile){
+
+  //get TTree from file ...
+  TreeReader data(inputFile.data());
+
+  Long64_t nTotal=0;
+  Long64_t nPass[20]={0};
+
+  TH1F* h_SD=new TH1F("h_SD","",100,0,200);
+  //Event loop
+  for(Long64_t jEntry=0; jEntry<data.GetEntriesFast() ;jEntry++){
+
+    if (jEntry % 50000 == 0)
+      fprintf(stderr, "Processing event %lli of %lli\n", jEntry + 1, data.GetEntriesFast());
+
+    data.GetEntry(jEntry);
+    nTotal++;
+
+    //0. has a good vertex
+    Int_t nVtx        = data.GetInt("nVtx");
+    if(nVtx<1)continue;
+    nPass[0]++;
+
+    int nFATJet         = data.GetInt("FATnJet");
+    const int nJets=nFATJet;
+    TClonesArray* fatjetP4 = (TClonesArray*) data.GetPtrTObject("FATjetP4");
+    Float_t*  fatjetCISVV2 = data.GetPtrFloat("FATjetCISVV2");
+    Float_t*  fatjetSDmass = data.GetPtrFloat("FATjetSDmass");
+    Int_t*   nSubSoftDropJet = data.GetPtrInt("FATnSubSDJet");
+    vector<float>   *subjetSDCSV =  data.GetPtrVectorFloat("FATsubjetSDCSV", nFATJet);
+    vector<float>   *subjetSDPx  =  data.GetPtrVectorFloat("FATsubjetSDPx", nFATJet);
+    vector<float>   *subjetSDPy  =  data.GetPtrVectorFloat("FATsubjetSDPy", nFATJet);
+    vector<float>   *subjetSDPz  =  data.GetPtrVectorFloat("FATsubjetSDPz", nFATJet);
+    vector<float>   *subjetSDE   =  data.GetPtrVectorFloat("FATsubjetSDCE", nFATJet);
+    
+    int nFatBTag=0;
+    for(int ij=0; ij<nJets; ij++)
+      {
+    	
+     	TLorentzVector* thisJet = (TLorentzVector*)fatjetP4->At(ij);
+    	if(thisJet->Pt()<30)continue;
+    	if(fabs(thisJet->Eta())>2.5)continue;
+    	if(fatjetSDmass[ij]<95 || fatjetSDmass[ij]>145)continue;
+
+    	if(fatjetCISVV2[ij] < 0.605)continue;
+    	if(fatjetCISVV2[ij] > 1)continue;
+
+    	nFatBTag++;
+
+      }
+    
+    if(nFatBTag>=2)nPass[1]++;
+
+
+    int nSubBTag[2]={0}; // how many fat jets have subjet b-tags
+    int nGoodFatJet=0;
+    for(int ij=0; ij<nJets; ij++)
+      {
+    	
+	TLorentzVector* thisJet = (TLorentzVector*)fatjetP4->At(ij);
+	if(thisJet->Pt()<30)continue;
+	if(fabs(thisJet->Eta())>2.5)continue;
+	if(fatjetSDmass[ij]<95 || fatjetSDmass[ij]>145)continue;
+
+
+      	for(int is=0; is < nSubSoftDropJet[ij]; is++){
+	  if(subjetSDCSV[ij][is] < 0.605)continue;
+	  if(nGoodFatJet<2)
+	  nSubBTag[nGoodFatJet]++;
+      	}
+
+	nGoodFatJet++;
+
+      }
+  
+    if(nSubBTag[0]==0 || nSubBTag[1]==0)continue;
+    if(nSubBTag[0]>0 && nSubBTag[1]>0)nPass[2]++;
+    if((nSubBTag[0]>1 && nSubBTag[1]>0) || 
+       (nSubBTag[0]>0 && nSubBTag[1]>1))nPass[3]++;
+    if(nSubBTag[0]>1 && nSubBTag[1]>1) nPass[4]++;
+        
+
+  } // end of loop over entries
+
+  std::cout << "nTotal    = " << nTotal << std::endl;
+  for(int i=0;i<20;i++)
+    if(nPass[i]>0)
+      std::cout << "nPass[" << i << "]= " << nPass[i] << std::endl;
+    
+}
