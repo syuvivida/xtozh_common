08c7643aee62d2c2b1ca2115ba509bf5466156d8
Shin-Shan Yu <syu@ncu200.cern.ch>
2015-11-26 11:47:50 +0100

    updated

--- selection_header_files/isPassZmumu.h
+++ selection_header_files/isPassZmumu.h
@@ -10,29 +10,33 @@ bool isPassZmumu(TreeReader &data, vector<Int_t>& goodMuID){
   
   goodMuID.clear();
 
-  Int_t    nMu       = data.GetInt("nMu");
-  Int_t*   muCharge  = data.GetPtrInt("muCharge");
-  Float_t* muMiniIso = data.GetPtrFloat("muMiniIso");
-  TClonesArray* muP4 = (TClonesArray*) data.GetPtrTObject("muP4");
-  vector<bool>& isHighPtMuon = *((vector<bool>*) data.GetPtr("isHighPtMuon"));
+  Int_t    nMu         = data.GetInt("nMu");
+  Int_t*   muCharge    = data.GetPtrInt("muCharge");
+  Float_t* muMiniIsoEA = data.GetPtrFloat("muMiniIsoEA");
+  TClonesArray* muP4   = (TClonesArray*) data.GetPtrTObject("muP4");
+  vector<bool>& isHighPtMuon        = *((vector<bool>*) data.GetPtr("isHighPtMuon"));
   vector<bool>& isCustomTrackerMuon = *((vector<bool>*) data.GetPtr("isCustomTrackerMuon"));
 
   // select good muons
       
   std::vector<Int_t> goodMuons;
-  
+  bool hasTrigMuon = false;  
+
   for(Int_t im = 0; im < nMu; im++){
       
     TLorentzVector* myMu = (TLorentzVector*)muP4->At(im);
 
+    if( muMiniIsoEA[im] > 0.2 ) continue;
+    if( !isHighPtMuon[im] && !isCustomTrackerMuon[im] ) continue;
     if( fabs(myMu->Eta()) > 2.4 ) continue;
     if( myMu->Pt() < 20 ) continue;
-    if( muMiniIso[im] >= 0.1 ) continue;
-    if( !isHighPtMuon[im] && !isCustomTrackerMuon[im] ) continue;
+    if( myMu->Pt() > 50 && fabs(myMu->Eta()) < 2.1 ) hasTrigMuon = true;
 
     goodMuons.push_back(im);
 
   }	
+
+  if( !hasTrigMuon ) goodMuons.clear();
 
   // select good Z boson
 
@@ -56,16 +60,9 @@ bool isPassZmumu(TreeReader &data, vector<Int_t>& goodMuID){
       Float_t ptll = (*thisMu+*thatMu).Pt();
 
       if( muCharge[im]*muCharge[jm] > 0 ) continue;
-      if( TMath::Max(pt1,pt2) < 50 ) continue;
       if( mll < 70 || mll > 110 ) continue;
       if( ptll < 200 ) continue;
       if( !( (isHighPtMuon[im] && isCustomTrackerMuon[jm]) || (isHighPtMuon[jm] && isCustomTrackerMuon[im]) ) ) continue;
-      if( pt1 > pt2 ){ 
-	if( fabs(thisMu->Eta()) > 2.1 ) continue;
-      }
-      if( pt2 > pt1 ){
-	if( fabs(thatMu->Eta()) > 2.1 ) continue;
-      }
 
       if( !findMPair ){
 
