927d2c5218e86c7536b9bbe61310bb5a01742c5b
Jerry <d3a2s1l@gmail.com>
2019-05-13 05:45:22 +0200

    upload

--- lxplus_HTcondor/skimming/subMacro.sh
+++ lxplus_HTcondor/skimming/subMacro.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #mode
-compile=0
+compile=1
 
 if [ $# -lt 2 ]
 then
@@ -17,11 +17,6 @@ export SCRAM_ARCH=slc6_amd64_gcc530
 export X509_USER_PROXY=/afs/cern.ch/user/s/shuxiao/private/grid.proxy
 echo "X509_USER_PROXY=$X509_USER_PROXY"
 # setting cmsenv
-scramv1 project CMSSW CMSSW_8_0_25  # cmsRel CMSSW_8_0_25
-cd CMSSW_8_0_25/src/
-eval $(scramv1 runtime -sh)         # cmsenv is an alias not on the workers
-cd ../..
-echo "Finish environment variables setting"
 
 # RUNNING
 if [[ $1 =~ ".so" ]]
@@ -33,10 +28,13 @@ void run() {
      gROOT->ProcessLine("${1%_C*}(\"$2\",\"$3\")");
 }
 EOF
+    echo ""
+    cat run.C
+    echo ""
     echo "running script..."
     root -b -q run.C
 
-elif [[ $1 =~ ".C" ]] || [[ $1 =~ ".c" ]]  # check for .C .cpp .cxx
+elif [[ $1 =~ ".C" ]] || [[ $1 =~ ".c" ]]  
 then
     echo "running macro..."
     if [ $compile -eq 1 ]
